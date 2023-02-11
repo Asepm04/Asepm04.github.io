931c38244b2391ced06f60af53f3aa8185dc3a0d
asep mulyadi <122654897+Asepm04@users.noreply.github.com>
2023-02-11 21:43:20 +0700

    Js

--- java.js
+++ java.js
@@ -28,10 +28,7 @@ window.addEventListener('scroll',function(){
   const value = window.scrollY;
   saya1.style.zIndex=10;
   saya1.style.top=value*0.7 + 'px';
-<<<<<<< HEAD
-  // aboutme1.style.left=value*0.9 +'px';
-=======
->>>>>>> refs/remotes/origin/main
+  aboutme1.style.left=value*0.9 +'px';
 
 
 
